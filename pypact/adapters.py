d8dce7588d72a3949eb8cc26e4c9086a4a2a5687
mcerah-bdt <metin.cerah@bigdatateknoloji.com>
2019-05-21 10:13:00 +0300

    Copy - paste mistake corrected.

--- pypact/adapters.py
+++ pypact/adapters.py
@@ -1,83 +1,150 @@
-"""This module make requests to the endpoints of Pact API.
-
-Functions:
-    - **send:** It makes a POST Request to Pact API's 'send' endpoint.
-    - **listen:** It makes a POST Request to Pact API's 'listen' \
-    endpoint.
-    - **local:** It makes a POST Request to Pact API's 'local' endpoint.
-    - **send_and_listen:** It combines send and listen functions.
-"""
-
+import datetime
+import hashlib
 import json
-import requests
 
-from .config import PACT_SERVER, PUB_KEY, PRIV_KEY
-from .adapters import BasePactAdapter
+import ed25519
 
 
-def _request(endpoint, body, json_=False):
-    if not json_:
-        return requests.post(PACT_SERVER + endpoint, body)
-    return requests.post(PACT_SERVER + endpoint, json=body)
+class BasePactAdapter:
+    @staticmethod
+    def generate_code_hash_and_sign(pact_code, pub_key, priv_key):
+        """Generate hash of pact code and signs it.
 
+        :param pact_code: The dictionary of Pact yaml file
+        :param pub_key: Public key of signing key
+        :param priv_key: Private key of signing key
+        :return: Returns tuple of hash of pact code and signed hash.
+        """
+        pact_code = bytes(pact_code, encoding="utf8")
+        hash2b = hashlib.blake2b()
+        hash2b.update(pact_code)
+        sk = ed25519.keys.SigningKey(bytes(priv_key, encoding="utf8"))
+        signing_key = priv_key + pub_key
+        sk.vk_s = bytes.fromhex(pub_key)
+        sk.sk_s = bytes.fromhex(signing_key)
 
-def send(pact_command, keyset):
-    """ Make a POST Request to Pact API's 'send' endpoint.
+        return hash2b.hexdigest(), sk.sign(hash2b.digest()).hex()
 
-    :param pact_command: Serialised pact command
-    :return: The result of POST request
-    """
-    req_body = json.loads(
-        BasePactAdapter.build_request(
-            pact_command, PUB_KEY, PRIV_KEY, keyset
+    @classmethod
+    def build_code(cls, module_name, func_name, keyset, **kwargs):
+        """It calls CommandFactory class in order to produce
+        Pact command.
+
+        :param module_name: The name of pact module which contains
+        function name
+        :param func_name: The name of function to be executed
+        :param keyset: Keyset to be used by function
+        :param kwargs: Additional parameters for pact function arguments
+        :return: Returns pact command
+        """
+        return CommandFactory(module_name, func_name, keyset, **kwargs).create()
+
+    @staticmethod
+    def build_request(pact_code, pub_key, priv_key, keyset_name, test_data=None):
+        cmd = {
+            "address": None,
+            "payload": {
+                "exec": {
+                    "data": {
+                        "testdata": test_data or "arbitrary user data",
+                        keyset_name: [pub_key],
+                    },
+                    "code": pact_code,
+                }
+            },
+            "nonce": str(datetime.datetime.now()),
+        }
+        pact_cmd = json.dumps(cmd)
+        hash_code, sig = BasePactAdapter.generate_code_hash_and_sign(
+            pact_cmd, pub_key, priv_key
         )
-    )
-    return _request("send", req_body, json_=True)
+        cmds = {
+            "cmds": [
+                {
+                    "hash": hash_code,
+                    "sigs": [{"sig": sig, "scheme": "ED25519", "pubKey": pub_key}],
+                    "cmd": pact_cmd,
+                }
+            ]
+        }
+        return json.dumps(cmds)
 
-def send_batch_commands(pact_command_list, keyset):
-    """ Make a POST Request to Pact API's 'send' endpoint.
+    @staticmethod
+    def build_batch_command_request_body(pact_codes, pub_key, priv_key,
+                                         keyset_name, test_data=None):
+        cmds = {
+            "cmds": []}
+        for single_code in pact_codes:
+            cmd = {
+                "address": None,
+                "payload": {
+                    "exec": {
+                        "data": {
+                            "testdata": test_data or "arbitrary user data",
+                            keyset_name: [pub_key],
+                        },
+                        "code": single_code,
+                    }
+                },
+                "nonce": str(datetime.datetime.now()),
+            }
+            pact_cmd = json.dumps(cmd)
+            hash_code, sig = BasePactAdapter.generate_code_hash_and_sign(
+                pact_cmd, pub_key, priv_key
+            )
+            single_cmd = {
+                "hash": hash_code,
+                "sigs": [{"sig": sig, "scheme": "ED25519", "pubKey": pub_key}],
+                "cmd": pact_cmd,
+            }
+            cmds["cmds"].append(single_cmd)
+        return json.dumps(cmds)
 
-    :param pact_command: Serialised pact command
-    :return: The result of POST request
+
+class CommandFactory:
+    """Pact command generator
+
+    Methods:
+        create: It is a generic method that produces pact commands.
     """
-    req_body = json.loads(
-        BasePactAdapter.build_batch_command_request_body(
-            pact_command_list, PUB_KEY, PRIV_KEY, keyset
-        )
-    )
-    return _request("send", req_body, json_=True)
 
-def listen(listen_key):
-    """Make a POST Request to Pact API's 'listen' endpoint.
+    def __init__(self, module_name, function_name, keyset, **kwargs):
+        """ Initiate instance variables.
 
-    :param listen_key: A key to be used for 'listen' endpoint body
-    :return: The result of POST request
-    """
-    return _request("listen", json.dumps({"listen": listen_key}))
+        :param module_name: The name of pact module which contains
+        function name
+        :param function_name: The name of function to be executed
+        :param keyset: Provided keyset for the transaction
+        :param kwargs: Additional parameters for pact function arguments
+        """
+        self.module_name = module_name
+        self.function_name = function_name
+        self.kwargs = kwargs
+        self.keyset = keyset
 
+    def _get_time_param(self):
+        """Returns formatted time if `time` key in kwargs otherwise None"""
+        try:
+            time_param = self.kwargs.pop("time")
+            return ' (time "{tm}")'.format(tm=time_param)
+        except KeyError:
+            pass
 
-def local(listen_key):
-    """Make a POST Request to Pact API's 'local' endpoint.
+    def create(self):
+        """ Generate pact command.
 
-    :param listen_key: A key to be used for 'local' endpoint body
-    :return: The result of POST request
-    """
-    return _request("local", json.dumps({"listen": listen_key}))
+        :return: Return pact command as a string
+        """
+        pact_command = f"(use '{self.module_name}) ({self.function_name}"
 
-
-def send_and_listen(pact_command, keyset):
-    """Call 'send' and 'listen' functions, respectively.
-
-    :param pact_command: Serialised pact command
-    :return: The result of POST request
-    """
-    resp = json.loads(send(pact_command, keyset).text)
-    listen_response_text = listen(resp["response"]["requestKeys"].pop()).text
-    response = json.loads(listen_response_text)["response"]
-
-    if "data" in response["result"]:
-        return response["result"]["data"]
-    elif "error" in response["result"]:
-        return response["result"]["detail"]
-
-    return response["result"]
+        # NOTE: make sure that you pass `time` parameter as last parameter to pact
+        # function
+        time_param = self._get_time_param()
+        for val in self.kwargs.values():
+            if isinstance(val, float):
+                param = str(val)
+            else:
+                param = str(val[2:]) if val[:2] == "/i" else '"' + val + '"'
+            pact_command += " " + param
+        command = pact_command + (time_param if time_param else "")
+        return command + ")"
